0ff36224b6fe330f83e96e704e891259b2ac4308
BhagyashriSabale <balisabale16@gmail.com>
2023-04-10 02:30:53 +0530

    [Bhagyashri] Calculating part time employees and there wage

--- Program.cs
+++ Program.cs
@@ -5,18 +5,26 @@
         static void Main(string[] args)
         {
             int empHrs = 0;
+            const int IS_FULL_TIME = 1;
+            const int IS_PART_TIME = 2;
             int empWage = 0;
             int isPresent = 1;
             const int EMP_RATE_PER_HR = 20;
+
             //welcome message
             Console.WriteLine("Welcome to Employee Wage Computation Problem Statement");
             //UC1-EMployeeAttendance
             Random random = new Random();
             int employeeAttendence = random.Next(0,2);
-            if (employeeAttendence == isPresent)
+            if (employeeAttendence == IS_FULL_TIME)
             {
-                Console.WriteLine("employee attendence is present");
+                Console.WriteLine("fulltime employee attendence is present");
                 empHrs = 8;
+            }
+            else if(employeeAttendence == IS_PART_TIME) 
+            {
+                Console.WriteLine("Parttime employee attendence is Present");
+                empHrs = 4;
             }
             else
             {
