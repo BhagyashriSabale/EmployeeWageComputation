6364dc26dd2dc7754c261edfa399f7cf7ffa9256
BhagyashriSabale <balisabale16@gmail.com>
2023-04-10 01:48:34 +0530

    [Bhagyashri] Calculating employee's wage

--- Program.cs
+++ Program.cs
@@ -4,20 +4,28 @@
     {
         static void Main(string[] args)
         {
+            int empHrs = 0;
+            int empWage = 0;
             int isPresent = 1;
+            const int EMP_RATE_PER_HR = 20;
             //welcome message
             Console.WriteLine("Welcome to Employee Wage Computation Problem Statement");
             //UC1-EMployeeAttendance
             Random random = new Random();
             int employeeAttendence = random.Next(0,2);
-            if (employeeAttendence == isPresent) 
+            if (employeeAttendence == isPresent)
             {
                 Console.WriteLine("employee attendence is present");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("employee attendence is absent");
+
             }
+            //UC2
+            empWage = EMP_RATE_PER_HR * empHrs;
+            Console.WriteLine("Employee wage is:" + empWage);
 
         }
     }
