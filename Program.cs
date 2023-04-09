fc7e32fd86484169c1da919b0f8de3b5eb6a9798
BhagyashriSabale <balisabale16@gmail.com>
2023-04-10 05:21:23 +0530

    [Bhagyashri] calculating wages for Noofworkingdays or Totalworkingpermonth

--- Program.cs
+++ Program.cs
@@ -6,18 +6,21 @@
         {
             int empHrs = 0;
             int day = 1;
+            int totalHrs = 0;
             const int IS_FULL_TIME = 1;
             const int IS_PART_TIME = 2;
             int empWage = 0;
             int totalWage = 0;
             const int EMP_RATE_PER_HR = 20;
             const int NUMBER_OF_WORKING_DAYS = 20;
+            const int MAX_WORKING_HRS = 100;
+            
 
             //welcome message
             Console.WriteLine("Welcome to Employee Wage Computation Problem Statement");
             //UC1-EMployeeAttendance
             Random random = new Random();
-            for (day = 1; day < NUMBER_OF_WORKING_DAYS; day++)
+           while(day <= NUMBER_OF_WORKING_DAYS && totalHrs <= MAX_WORKING_HRS)
             {
                 int employeeAttendence = random.Next(0, 3);
                 switch (employeeAttendence)
@@ -35,10 +38,12 @@
 
                 //UC2
                 empWage = EMP_RATE_PER_HR * empHrs;
-                Console.WriteLine("Employee wage for day{0} is:{1}",day,empWage);
+                Console.WriteLine("Employee wage for day {0} and {1}hrs is:{2}",day,empHrs,empWage);
+                day++;
+                totalHrs += empHrs;
                 totalWage += empWage;
             }
-            Console.WriteLine("TotalWage for {0} days is:{1}",(day - 1), totalWage);
+            Console.WriteLine("TotalWage for {0} days and {1} hrs  is:{2}",(day - 1),totalHrs,totalWage);
 
         }
     }
